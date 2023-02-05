01850d5f23d4a82132340a05d553e029c44c08ba
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-05 13:24:07 -0600

    Add tests for #append for JungleBeat class

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -18,4 +18,24 @@ RSpec.describe JungleBeat do
       expect(@jb.list.head).to eq(nil)
     end
   end
+
+  describe '#append' do
+    it 'appends 3 elements correctly' do
+      @jb.append('deep doo ditt')
+      expect(@jb.list.head.data).to eq('deep')
+      expect(@jb.list.head.next_node.data).to eq('doo')
+      expect(@jb.list.head.next_node.next_node.data).to eq('ditt')
+      expect(@jb.list.to_string).to eq('deep doo ditt')
+    end
+
+    it 'appends an additional 3 elements correctly' do
+      @jb.append('deep doo ditt')
+      @jb.append('woo hoo shu')
+      expect(@jb.list.to_string).to eq('deep doo ditt woo hoo shu')
+    end
+  end
+
+  describe '#count' do
+
+  end
 end
