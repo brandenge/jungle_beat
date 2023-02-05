c08c5c540e27a2e2e9a131e076152de145967e68
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-05 13:35:51 -0600

    Add #count for JungleBeat class

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -6,4 +6,21 @@ class JungleBeat
   def initialize
     @list = LinkedList.new
   end
+
+  def append(string_data)
+    data_array = string_data.split(' ')
+    data_array.each do |data|
+      @list.append(data)
+    end
+  end
+
+  def count
+    count = 0
+    current = @list.head
+    while current != nil
+      current = current.next_node
+      count += 1
+    end
+    count
+  end
 end
