f4b46d80234db30be0ae0a368585360c589a3c98
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-07 12:14:44 -0600

    Add: #all, #prepend, #reset_rate and #reset_voice

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,17 +1,24 @@
 require './lib/linked_list'
 
 class JungleBeat
+  BEATS = %w(boop bop dee deep ditt doo doop dop hoo la na plop shi shu suu tee woo).freeze
+
   attr_reader :list
+  attr_accessor :rate, :voice
 
   def initialize
     @list = LinkedList.new
+    @rate = 500
+    @voice = 'Boing'
   end
 
   def append(string_data)
-    data_array = string_data.split(' ')
+    data_array = string_data.split
+    data_array = data_array.filter{ |data| BEATS.include?(data) }
     data_array.each do |data|
       @list.append(data)
     end
+    data_array.count
   end
 
   def count
@@ -28,6 +35,27 @@ class JungleBeat
     command = 'say -r 500 -v Boing'
     beats = @list.to_string
     `#{command} #{beats}`
-    "#{command} #{beats}"
+    count
+  end
+
+  def all
+    @list.to_string
+  end
+
+  def prepend(string_data)
+    data_array = string_data.split
+    data_array = data_array.filter{ |data| BEATS.include?(data) }.reverse
+    data_array.each do |data|
+      @list.prepend(data)
+    end
+    data_array.count
+  end
+
+  def reset_rate
+    @rate = 500
+  end
+
+  def reset_voice
+    @voice = 'Boing'
   end
 end
